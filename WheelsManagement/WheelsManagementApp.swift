2abfe488f6367e68c89b94af9dc2e23f81057093
mohammedichou <devichoumohammed@gmail.com>
2023-11-10 11:10:34 +0100

    Initial Commit

---
+++ WheelsManagement/WheelsManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelsManagementApp.swift
+//  WheelsManagement
+//
+//  Created by mohammed ichou on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WheelsManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
